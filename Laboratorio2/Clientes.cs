25dc13ff74f713f9758bf4c1acdae59973ca074f
Caio <caioboudens00@gmail.com>
2021-03-23 22:32:17 -0300

    Exercício completado

---
+++ Laboratorio2/Clientes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio2
+{
+    class Clientes
+    {
+        public string nomeUsuario { get; set; }
+        public string numeroIdentificacao { get;set; }
+        public float saldo { get; protected set; }
+        public bool tipoConta { get; set; }
+ 
+        //Considerando true para Conta-Corrente e false para Conta-Poupança.
+
+        public void depositar(Clientes cliente, float quantidadeDeposito)
+        {
+            this.saldo += quantidadeDeposito;
+            Console.WriteLine("O deposito de R$ {0} na conta {1} foi realizado com sucesso.", quantidadeDeposito, cliente.numeroIdentificacao);
+        }
+
+        public void consultarSaldo(Clientes cliente)
+        {
+            Console.WriteLine("O saldo da conta {0} é de R$ {1}.", cliente.numeroIdentificacao, cliente.saldo);
+        }
+
+        public void sacar(Clientes cliente, float valorSaque)
+        {
+            if (valorSaque > saldo)
+            {
+                saldo = saldo;
+                Console.WriteLine("Não foi possível realizar o saque, não há saldo suficiente");
+            }
+            else if (saldo > 0)
+            {
+                if (cliente.tipoConta == true)
+                {
+
+                    this.saldo -= (valorSaque + (valorSaque * 0.0037f));
+                }
+                else
+                {
+                    this.saldo -= (valorSaque + (valorSaque * 0.0020f));
+                }
+                Console.WriteLine("Foi sacado R$ {0} da conta {1}.", valorSaque, cliente.numeroIdentificacao);
+            }
+
+        }
+
+        public void transferir(float valor , Clientes destinatario)
+        {
+            if (valor > this.saldo)
+            {
+                saldo = saldo;
+                Console.WriteLine("Não foi possível realizar a transferência, não há saldo suficiente");
+            }
+            else if (this.saldo > 0)
+            {
+                if (this.tipoConta == true)
+                {
+
+                    this.saldo -= (valor + (valor * 0.0010f));
+                }
+                else
+                {
+                    this.saldo -= (valor + (valor * 0.0015f));
+                }
+                Console.WriteLine("Foi transferido R$ {0} da conta {1} para a conta {2}.", valor, this.numeroIdentificacao,destinatario.numeroIdentificacao);
+                destinatario.saldo += valor;
+            }
+
+        }
+    }
+}
--- Laboratorio2/Program.cs
+++ Laboratorio2/Program.cs
@@ -6,7 +6,56 @@ namespace Laboratorio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("22");
+            //Considerando true para Conta-Corrente e false para Conta-Poupança.
+
+            Clientes Bob = new Clientes();
+            Bob.nomeUsuario = "Bob Nelson";
+            Bob.numeroIdentificacao = "12345-6";
+            Bob.tipoConta = true;
+
+
+            Clientes Testolfo = new Clientes();
+            Testolfo.nomeUsuario = "Testolfo Rocha";
+            Testolfo.numeroIdentificacao = "71717-1";
+            Testolfo.tipoConta = false;
+
+            Clientes Lisa = new Clientes();
+            Lisa.nomeUsuario = "Lisa Leite";
+            Lisa.numeroIdentificacao = "65432-1";
+            Lisa.tipoConta = true;
+
+            //Depositos
+            Bob.depositar(Bob, 5000);
+            Testolfo.depositar(Testolfo, 1500);
+            Lisa.depositar(Lisa, 2000);
+            //Transferência de Bob para Testolfo
+            Bob.transferir(600,Testolfo);
+            //Saque Lisa
+            Lisa.sacar(Lisa, 250);
+            //Transferência de Lisa para Testolfo
+            Lisa.transferir(400, Testolfo);
+            //Transferência Testolfo para Bob
+            Testolfo.transferir(1000, Bob);
+            //Saque Bob
+            Bob.sacar(Bob, 900);
+            //Transferência de Bob para Lisa
+            Bob.transferir(1500,Lisa);
+            //Transferência de Testolfo para Lisa
+            Testolfo.transferir(1200, Lisa);
+            //Saque Bob
+            Bob.sacar(Bob, 2000);
+            //Deposito Lisa
+            Lisa.depositar(Lisa, 100);
+            //Transferência Testolfo para Bob
+            Testolfo.transferir(700, Bob);
+
+
+            Console.WriteLine("SALDOS FINAIS:");
+            Bob.consultarSaldo(Bob);
+            Testolfo.consultarSaldo(Testolfo);
+            Lisa.consultarSaldo(Lisa);
+
+            Console.ReadLine();
         }
     }
 }
